5fe45e2cf135f96671180a9e05ea64e8daed63fa
ttbarnes <tony@tonybarnes.me>
2021-10-01 23:30:50 +0100

    active nav links

--- components/Header/NavLink.js
+++ components/Header/NavLink.js
@@ -1,12 +1,29 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router'
 import styles from './NavLink.module.css';
 
-const NavLink = ({ link, text}) => (
-  <Link href={link}>
-    <a className={styles.navLink}>
-     <span className={styles.navLinkInner}>{text}</span>
-   </a>
-  </Link>
-);
+const NavLink = ({ link, text}) => {
+  const router = useRouter();
+
+  const { pathname } = router;
+
+  let isActive;
+
+  if (link === '/' && pathname === '/') {
+    isActive = true;
+  } else if (link !== '/') {
+    isActive = pathname.includes(link);
+  }
+
+  const linkClassName = isActive ? `${styles.navLink} ${styles.navLinkActive}` : styles.navLink;
+
+  return (
+    <Link href={link}>
+      <a className={linkClassName}>
+      <span className={styles.navLinkInner}>{text}</span>
+    </a>
+    </Link>
+  );
+};
 
 export default NavLink;
