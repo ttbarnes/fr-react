5e022dab8a51c7c513ada7c4d9402b5ffdb94107
ttbarnes <tony@tonybarnes.me>
2021-10-02 11:31:01 +0100

    fav/app icons

--- pages/_app.js
+++ pages/_app.js
@@ -22,6 +22,8 @@ const App = ({ Component, pageProps }) => {
         <meta name='mobile-web-app-capable' content='yes' />
         <meta name='apple-mobile-web-app-capable' content='yes' />
         <meta name='apple-mobile-web-app-status-bar-style' content='black' />
+        <meta name='msapplication-TileColor' content='#000' />
+        <meta name='msapplication-TileImage' content='/ms-icon-144x144.png' />
         <meta name='theme-color' content='#000' />
 
 
@@ -53,6 +55,23 @@ const App = ({ Component, pageProps }) => {
         <meta name='google-site-verification' content={CONSTANTS.META_TAGS.GOOGLE_SITE_VERIFICATION_2} />
         <meta name='google-site-verification' content={CONSTANTS.META_TAGS.GOOGLE_SITE_VERIFICATION_3} />
         <meta name='msvalidate.01' content={CONSTANTS.META_TAGS.MICROSOFT_VERFICATION} />
+
+
+        {/* favicon's generated via favicon-generator.org */}
+        <link rel='apple-touch-icon' sizes='57x57' href={`${CONSTANTS.BASE_URL}/apple-icon-57x57.png`} />
+        <link rel='apple-touch-icon' sizes='60x60' href={`${CONSTANTS.BASE_URL}/apple-icon-60x60.png`} />
+        <link rel='apple-touch-icon' sizes='72x72' href={`${CONSTANTS.BASE_URL}/apple-icon-72x72.png`} />
+        <link rel='apple-touch-icon' sizes='76x76' href={`${CONSTANTS.BASE_URL}/apple-icon-76x76.png`} />
+        <link rel='apple-touch-icon' sizes='114x114' href={`${CONSTANTS.BASE_URL}/apple-icon-114x114.png`} />
+        <link rel='apple-touch-icon' sizes='120x120' href={`${CONSTANTS.BASE_URL}/apple-icon-120x120.png`} />
+        <link rel='apple-touch-icon' sizes='144x144' href={`${CONSTANTS.BASE_URL}/apple-icon-144x144.png`} />
+        <link rel='apple-touch-icon' sizes='152x152' href={`${CONSTANTS.BASE_URL}/apple-icon-152x152.png`} />
+        <link rel='apple-touch-icon' sizes='180x180' href={`${CONSTANTS.BASE_URL}/apple-icon-180x180.png`} />
+        <link rel='icon' type='image/png' sizes='192x192' href={`${CONSTANTS.BASE_URL}/android-icon-192x192.png`} />
+        <link rel='icon' type='image/png' sizes='32x32' href={`${CONSTANTS.BASE_URL}/favicon-32x32.png`} />
+        <link rel='icon' type='image/png' sizes='96x96' href={`${CONSTANTS.BASE_URL}/favicon-96x96.png`} />
+        <link rel='icon' type='image/png' sizes='16x16' href={`${CONSTANTS.BASE_URL}/favicon-16x16.png`} />
+        <link rel='shortcut icon' href={`${CONSTANTS.BASE_URL}/favicon.ico`} type='image/x-icon' />
       </Head>
 
       <Header />
