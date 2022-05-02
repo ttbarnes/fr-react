223a0e6beec175ec5ff63f44b74135287e636bfc
ttbarnes <tony@tonybarnes.me>
2022-05-02 16:41:00 +0100

    send origin header in graphql queries

--- apollo-client.js
+++ apollo-client.js
@@ -1,8 +1,21 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink
+} from '@apollo/client';
+
+const link = createHttpLink({
+  uri: process.env.GRAPHQL_API,
+  credentials: 'include',
+  headers: {
+    origin: process.env.NEXT_PUBLIC_BASE_URL
+  }
+});
 
 const client = new ApolloClient({
   uri: process.env.GRAPHQL_API,
   cache: new InMemoryCache(),
+  link
 });
 
 export default client;
