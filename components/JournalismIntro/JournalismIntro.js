02b026b0f244efdbfdaa325aaaaf7e4ef669b048
ttbarnes <tony@tonybarnes.me>
2023-11-12 18:10:52 +0000

    chore(journalism): copy updates

--- components/JournalismIntro/JournalismIntro.js
+++ components/JournalismIntro/JournalismIntro.js
@@ -44,6 +44,26 @@ const LinkBAJunion = () => (
   <a className='link-highlight-hover-border' href='https://bajunion.org.uk/' target='_blank' rel='noopener noreferrer'>The British Association of Journalists</a>
 );
 
+const LinkTCTTLTopPodcast = () => (
+  <a className='link-highlight-hover-border' href='https://podcasts.feedspot.com/uk_music_podcasts/?feedid=5573991' target='_blank' rel='noopener noreferrer'>Top 60 UK Music Podcasts on the web</a>
+);
+
+const LinkTCTTLapple = () => (
+  <a className='link-highlight-hover-border' href='https://podcasts.apple.com/gb/podcast/thoughts-conversations-and-to-do-lists/id1685136847?ign-itscg=30200&ign-itsct=podtail_podcasts' target='_blank' rel='noopener noreferrer'>Apple</a>
+);
+
+const LinkTCTTLspotify = () => (
+  <a className='link-highlight-hover-border' href='https://open.spotify.com/show/6DYV6dMGODtj0A2SBxgUaP' target='_blank' rel='noopener noreferrer'>Spotify</a>
+);
+
+const LinkTCTTLukPodcasts = () => (
+  <a className='link-highlight-hover-border' href='https://uk-podcasts.co.uk/podcast/thoughts-conversations-and-to-do-lists' target='_blank' rel='noopener noreferrer'>UK Podcasts</a>
+);
+
+const LinkTCTTLgoogle = () => (
+  <a className='link-highlight-hover-border' href='https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy9kZDcyNDhlNC9wb2RjYXN0L3Jzcw' target='_blank' rel='noopener noreferrer'>Google</a>
+);
+
 const LinkWomenInJournalism = () => (
   <a className='link-highlight-hover-border' href='https://womeninjournalism.co.uk/' target='_blank' rel='noopener noreferrer'>womeninjournalism.co.uk</a>
 );
@@ -62,14 +82,14 @@ const LinkWIJMemail = () => (
 
 const JournalismIntro = () => (
   <div className={styles.container}>
-    <p>As well as working as a Jazz artist, Fiona spends her time behind the scenes in Jazz working as the founder of <LinkWIJM />, senior writer for <LinkJazzInEurope /> and writing for publications including <LinkKindOfJazz />, <LinkJazzNorth />, <LinkJazzUk /> and <LinkConnectsMusic />.</p>
+    <p>As well as working as a Jazz artist, Fiona spends her time behind the scenes in Jazz working as the founder of <LinkWIJM />, senior writer for <LinkJazzInEurope /> and writing for publications including <LinkKindOfJazz />, <LinkJazzNorth />, <LinkJazzUk /> and <LinkConnectsMusic />. Fiona was the guest editor in chief for the January 2021 edition of <LinkJazzQuarterly />, where she was responsible for a rarely seen fully diverse and gender balanced Jazz publication.</p>
 
-    <p>Fiona was the guest editor in chief for the January 2021 edition of <LinkJazzQuarterly />, where she was responsible for a rarely seen fully diverse and gender balanced Jazz publication.</p>
-
-    <p>Fiona has interviewed an impressive list of inspirational artists including Rickie Lee Jones, Brandee Younger, Steve Gadd, Michel Camilo, Kyle Eastwood, Dee Dee Bridgewater, Maxine Gordon, Terri Lyne Carrington, Barbara Thompson OBE, Ruby Turner, Hiromi, Camille Thurman, Celine Peterson, Hiromi, Jordannah Elizabeth and Dr Tammy Kernodle.</p>
+    <p>Fiona has interviewed an impressive list of inspirational artists including Rickie Lee Jones, Brandee Younger, Jazzmeia Horn,Steve Gadd, Michel Camilo, Kyle Eastwood, Dee Dee Bridgewater, Maxine Gordon, Terri Lyne Carrington, Barbara Thompson OBE, Ruby Turner, Hiromi, Camille Thurman, Celine Peterson, Hiromi, Jordannah Elizabeth and Dr Tammy Kernodle.</p>
 
     <p>As founder of the ground breaking award winning organisation, <LinkWIJM />, Fiona has developed many exciting initiatives including a mentoring scheme for new female writers, the 'On The Bookcase' series, a range of <LinkWIJMpodcasts /> and is the Editor in Chief of the acclaimed Women in Jazz Media magazine giving platforms to women working in Jazz across the world. Through Women in Jazz Media, Fiona collaborates with many official partners including <LinkBLIM /> and Céline Peterson and her work with these partners has led to an increase in the gender and diversity balance in several publications across the world.</p>
 
+
+    <p>Fiona launched her new podcast series 'Thoughts, Conversations and To Do Lists' in April 2023 inspired by her recent album title and 'Every podcast guest is someone who inspires me and who I have previously had a wonderful conversation with'. With no theme, the field is open for a wide range of fascinating conversations! This podcast series has recently been rated on FeedSpot as number 8 in the <LinkTCTTLTopPodcast />. You can listen to the podcast on all major platforms including <LinkTCTTLapple /> <LinkTCTTLspotify /> <LinkTCTTLukPodcasts /> and <LinkTCTTLgoogle />.</p>
     <p>Fiona is a member of the <LinkJazzJournalists />, <LinkBAJunion /> and <LinkWomenInJournalism />.</p>
 
     <p>Follow Fiona's journalism on her <LinkInstagramJournalism /></p>
