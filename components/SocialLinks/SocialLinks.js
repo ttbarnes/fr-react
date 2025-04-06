025a1ac96db1296aed57460334457d1fab9a7175
ttbarnes <tony@tonybarnes.me>
2025-04-06 10:52:51 +0100

    style(social): update social media icons

--- components/SocialLinks/SocialLinks.js
+++ components/SocialLinks/SocialLinks.js
@@ -32,17 +32,54 @@ const SocialLinks = ({
       </a>
     </li>
 
-
     <li className={isSticky ? `${styles.listItem} ${styles.isSticky}` : styles.listItem}>
       <a
-        className={styles.listItemLink}
-        href={CONSTANTS.SOCIAL_MEDIA_URLS.TWITTER}
+        className={`${styles.listItemLink} ${styles.iconTtLink}`}
+        href={CONSTANTS.SOCIAL_MEDIA_URLS.TIKTOK}
         target='_blank'
         rel='noopener noreferrer'
-        aria-label='Twitter'
+        aria-label='TikTok'
       >
-        <i className={`icon-s-t ${styles.listItemIcon}`}></i>
-        <span className={styles.text}>Twitter</span>
+        <img
+          src={'/images/icons/s-tt.svg'}
+          alt='TikTok'
+          className='icon-tt'
+          aria-label='TikTok'
+        />
+      </a>
+    </li>
+
+    <li className={isSticky ? `${styles.listItem} ${styles.isSticky} ${styles.iconThr}` : `${styles.listItem} ${styles.iconThr}`}>
+      <a
+        className={`${styles.listItemLink} ${styles.iconThr}`}
+        href={CONSTANTS.SOCIAL_MEDIA_URLS.THREADS}
+        target='_blank'
+        rel='noopener noreferrer'
+        aria-label='Threads'
+      >
+        <img
+          src={'/images/icons/s-thr.svg'}
+          alt='Threads'
+          className='icon-thr'
+          aria-label='Threads'
+        />
+      </a>
+    </li>
+
+    <li className={isSticky ? `${styles.listItem} ${styles.isSticky} ${styles.iconBs}` : `${styles.listItem} ${styles.iconBs}`}>
+      <a
+        className={`${styles.listItemLink}`}
+        href={CONSTANTS.SOCIAL_MEDIA_URLS.BLUESKY}
+        target='_blank'
+        rel='noopener noreferrer'
+        aria-label='Bluesky'
+      >
+        <img
+          src={'/images/icons/s-bs.svg'}
+          alt='Bluesky'
+          className='icon-bs'
+          aria-label='Bluesky'
+        />
       </a>
     </li>
 
--- constants/index.js
+++ constants/index.js
@@ -5,7 +5,7 @@ const META_TAGS = {
   DESCRIPTION: 'Award Winning Vocalist, Pianist, Composer, Producer and Journalist. 7 Songs in 7 Days out now',
   KEYWORDS: 'Fiona Ross, jazz, vocalist, pianist, composer, musician, singer, songwriter, producer, journalist, london',
   AUTHOR: 'Fiona Ross',
-  OG_DESCRIPTION: 'It\’s just me doing my thing',
+  OG_DESCRIPTION: "It\'s just me doing my thing",
   TWITTER_CARD: 'summary_large_image',
   FACEBOOK_APP_ID: '1779328982334234',
   GOOGLE_SITE_VERIFICATION_1: 'vXuozyxCB7mdGABhxU9iHpi7J3Im4arLmV9Vsafglek',
@@ -85,10 +85,12 @@ const SOCIAL_MEDA_IMAGE_SIZE = {
 const SOCIAL_MEDIA_URLS = {
   BANDCAMP: 'https://fionarossjazz.bandcamp.com',
   SPOTIFY: 'https://play.spotify.com/artist/59lPcyDIZJxmRqatvAjct8?play=true',
-  TWITTER: 'http://twitter.com/fifross',
   FACEBOOK: 'http://facebook.com/fionarossmusic',
   INSTAGRAM: 'https://www.instagram.com/fifi_ross',
-  YOUTUBE: 'https://www.youtube.com/channel/UC9srPEZZNgnsM23Q777HbzA'
+  YOUTUBE: 'https://www.youtube.com/channel/UC9srPEZZNgnsM23Q777HbzA',
+  TIKTOK: 'https://www.tiktok.com/@fifross',
+  THREADS: 'https://www.threads.net/@fifi_ross',
+  BLUESKY: 'https://bsky.app/profile/fifiross.bsky.social',
 };
 
 const JOURNALISM_CATEGORIES = {
