16845b5b8cdaa94e474297913609e414e83425ec
ttbarnes <tony@tonybarnes.me>
2026-05-30 19:34:27 +0100

    chore(deps): revert apollo/client bump

--- apollo-client.js
+++ apollo-client.js
@@ -1,11 +1,7 @@
 import {
   ApolloClient,
-  InMemoryCache,
   HttpLink
 } from '@apollo/client';
-
-import { Defer20220824Handler } from '@apollo/client/incremental';
-import { LocalState } from '@apollo/client/local-state';
 
 const link = new HttpLink({
   uri: process.env.GRAPHQL_API,
@@ -16,13 +12,8 @@ const link = new HttpLink({
 });
 
 const client = new ApolloClient({
+  uri: process.env.GRAPHQL_API,
   link,
-  cache: new InMemoryCache(),
-  link: new HttpLink({}),
-  localState: new LocalState({}),
-  incrementalHandler: new Defer20220824Handler(),
-  link: new HttpLink({}),
-  localState: new LocalState({}),
 });
 
 export default client;
